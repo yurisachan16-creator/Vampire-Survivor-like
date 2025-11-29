1c931a2918910a02785a9d5a44c6ea7b3ee20d26
Yurisa-Chan1 <1533843271@qq.com>
2025-11-30 06:39:55 +0800

    first commit

---
+++ Assets/Scripts/Game/Player.Designer.cs
@@ -0,0 +1,9 @@
+// Generate Id:032b4cfa-f567-41dc-b5ef-4e08a25f2c27
+using UnityEngine;
+
+namespace VampireSurvivorLike
+{
+	public partial class Player
+	{
+	}
+}
---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using QFramework;
+
+namespace VampireSurvivorLike
+{
+	public partial class Player : ViewController
+	{
+		void Start()
+		{
+			// Code Here
+			"Hello, Vampire Survivor Like Player!".LogInfo();
+		}
+	}
+}
